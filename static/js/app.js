fcba4a49e4a89ee0e43371ded8b8dccc8bf6eacd
Daniel <dconde@alumni.scu.edu>
2019-09-23 17:52:40 -0700

    fixed tool tip on scatter

--- static/js/app.js
+++ static/js/app.js
@@ -149,7 +149,7 @@ function updateToolTip(chosenXAxis, chosenYAxis, circlesGroup) {
   circlesGroup.call(toolTip);
 
   circlesGroup.on("mouseover", function(data) {
-    toolTip.show(data);
+    toolTip.show(data, this);
   })
     // onmouseout event
     .on("mouseout", function(data, index) {
